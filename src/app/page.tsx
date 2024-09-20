7a6639f250013a09fc26c3b368ae6fb38187f1db
Faraz Ali <farazali9028@gmail.com>
2024-09-20 17:00:07 +0530

    basic Drawboard created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  // creating a HTML canvas to draw and using useref so that our component doesn't re-render
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  // if user is drawing or not 
+  const [isDrawing, setIsDrawing] = useState(false)
+
+  useEffect(() => {
+    // initializing the canvas
+    const canvas = canvasRef.current
+
+    if (canvas) {
+      const ctx = canvas.getContext('2d')
+      if (ctx) {
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight - canvas.offsetTop
+        ctx.lineCap = "round" // for brush type 
+        ctx.lineWidth = 3 // for brush size
+      }
+    }
+  }, [])
+
+
+  // when user wants to start drawing this function runs takes mouseEvent as an argument
+  const StartDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      // setting the bgc and setting the 2d context
+      canvas.style.backgroundColor = 'black';
+
+      const ctx = canvas.getContext('2d')
+
+      if (ctx) {
+        ctx.beginPath()
+        ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        setIsDrawing(true)
+      }
+    }
+  }
+
+  const StopDrawing = () => {
+    setIsDrawing(false)
+  }
+
+  // Function to implement the drawing method
+  const Draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing) {
+      return
+    }
+    // we only draw when isDrawing is true
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const ctx = canvas.getContext("2d")
+      if (ctx) {
+        // white brush, Line to to follow our brush and stroke function to draw whereever the lineto function is called
+        ctx.strokeStyle = "white"
+        ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+        ctx.stroke();
+      }
+    }
+  }
+
+  return (
+    <canvas
+      ref={canvasRef}
+      id="canvas"
+      className="absolute top-0 left-0 w-full h-full"
+
+      onMouseDown={StartDrawing}
+      onMouseMove={Draw}
+      onMouseOut={StopDrawing}
+      onMouseUp={StopDrawing}
+    />
   );
 }
